9cfc05eea99a0f475bf0585ba407dfe3b48b39ef
IRHM <37304121+IRHM@users.noreply.github.com>
2023-10-19 22:35:59 +0100

    Sidebar: Allow resizing with a dragbar

--- client/components/Navigation/Sidebar/index.tsx
+++ client/components/Navigation/Sidebar/index.tsx
@@ -1,8 +1,7 @@
 'use client';
 import { usePathname } from 'next/navigation';
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import { AiOutlineHome } from 'react-icons/ai';
-import { BiArrowBack } from 'react-icons/bi';
 import { MdOutlineAudioFile, MdOutlineFavorite } from 'react-icons/md';
 import { twMerge } from 'tailwind-merge';
 
@@ -47,6 +46,27 @@ const Sidebar = ({ children }: SidebarProps) => {
     ],
     [pathname]
   );
+  const sidebar = useRef<HTMLDivElement>(null);
+  const resizeDragger = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (resizeDragger.current) {
+      resizeDragger.current.addEventListener('mousedown', () => {
+        document.addEventListener('mousemove', resize, false);
+        document.addEventListener(
+          'mouseup',
+          () => {
+            document.removeEventListener('mousemove', resize, false);
+          },
+          false
+        );
+      });
+    }
+  }, []);
+
+  const resize = (e: MouseEvent) => {
+    sidebar.current!.style.width = `${e.x}px`;
+  };
 
   return (
     <div
@@ -55,14 +75,14 @@ const Sidebar = ({ children }: SidebarProps) => {
         player.activeId && 'h-[calc(100%-80px)]'
       )}
     >
-      <div className="hidden h-full w-[250px] select-none flex-col gap-y-2 bg-sidebar-background px-4 py-2 md:flex">
+      <div
+        ref={sidebar}
+        className="relative h-full w-[250px] max-w-[400px] select-none flex-col gap-y-2 bg-sidebar-background px-4 py-2 md:flex"
+      >
         {/* <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2"> */}
 
         <div className="relative flex flex-row items-center justify-center">
           <p className="">Melodiy</p>
-          <div className="absolute right-0 top-0 cursor-pointer py-[3px] hover:text-inactive">
-            <BiArrowBack size={16} />
-          </div>
         </div>
 
         <div className="flex flex-col gap-y-1 py-4">
@@ -78,6 +98,12 @@ const Sidebar = ({ children }: SidebarProps) => {
           {/* Nested as <PlaylistBody> will become a server component. */}
           <PlaylistBody />
         </PlaylistHeader>
+
+        {/* Resize dragger */}
+        <div
+          ref={resizeDragger}
+          className="bg-green absolute right-[-5px] top-0 h-full w-[20px] cursor-col-resize"
+        ></div>
       </div>
 
       <main className="h-full flex-1 overflow-y-auto px-4 py-2 pr-5">
