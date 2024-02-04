e36194f5571c62e2f5490f781ecb0d527df29623
DNYLA <22224922+DNYLA@users.noreply.github.com>
2024-02-04 12:05:25 +0000

    renamed + added nx monorepo

---
+++ frontend/libs/shared/network/src/lib/network.tsx
@@ -0,0 +1,14 @@
+import './network.css';
+
+/* eslint-disable-next-line */
+export interface NetworkProps {}
+
+export function Network(props: NetworkProps) {
+  return (
+    <div>
+      <h1>Welcome to Network!</h1>
+    </div>
+  );
+}
+
+export default Network;
