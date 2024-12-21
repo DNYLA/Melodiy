56ef7f2a470690892c346237f5eb387ee2075d92
DNYLA <22224922+DNYLA@users.noreply.github.com>
2024-12-21 10:53:20 +0000

    Added Tailwind v4

--- client_new/apps/web/src/App.tsx
+++ client_new/apps/web/src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css';
 import { Button } from '@melodiy/ui';
 
 function App() {
   return (
-    <>
+    <div className="bg-base-accent">
       <Button />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
--- client_new/packages/ui/src/Button.tsx
+++ client_new/packages/ui/src/Button.tsx
@@ -1,3 +1,15 @@
+import { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+
 export default function Button() {
-  return <div>Button</div>;
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <button
+      className={twMerge('bg-modal', isActive && 'bg-emerald-400')}
+      onClick={() => setIsActive(!isActive)}
+    >
+      Button
+    </button>
+  );
 }
