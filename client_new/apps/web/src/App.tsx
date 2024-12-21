8b5d144b669caa922088d69a7c80752b2145813b
DNYLA <22224922+DNYLA@users.noreply.github.com>
2024-12-21 10:43:54 +0000

    Created PNPM project with react app + UI library

---
+++ client_new/apps/web/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from '@melodiy/ui';
+
+function App() {
+  return (
+    <>
+      <Button />
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
