07b0cc841ba883e6239a178e1c73a3bee3a6f2bf
DNYLA <22224922+DNYLA@users.noreply.github.com>
2024-12-21 12:00:28 +0000

    Added Api Library

---
+++ client_new/packages/api/src/index.ts
@@ -0,0 +1,13 @@
+export * from './lib/collections/album';
+export * from './lib/collections/playlist';
+
+export * from './lib/admin';
+export * from './lib/artist';
+export * from './lib/player';
+export * from './lib/search';
+export * from './lib/track';
+export * from './lib/user';
+
+export * from './utils';
+
+export * from './axios';
